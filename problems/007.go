e3f08da30180a0a6aeb5c33ea7bb141a13942513
Dan Couture <MathYourLife@gmail.com>
2015-04-29 21:58:11 -0400

    Don't need to check known primes > sqrt(candidate)

--- problems/007.go
+++ problems/007.go
@@ -8,12 +8,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type PrimeGenerator struct {
 	primes []uint64
 	current []uint64
 	n uint64
+	threshold uint64
 }
 
 func NewPrimeGenerator() *PrimeGenerator {
@@ -21,6 +23,7 @@ func NewPrimeGenerator() *PrimeGenerator {
 		primes: make([]uint64, 0),
 		current: make([]uint64, 0),
 		n: 2,
+		threshold: uint64(math.Sqrt(2)),
 	}
 }
 
@@ -49,6 +52,9 @@ CurrentLoop:
 				p.current[i] = c
 				break CurrentLoop
 
+			} else if p.primes[i] > p.threshold {
+				break CurrentLoop
+
 			} else if c < p.n {
 				c += p.primes[i]
 				continue
@@ -60,6 +66,7 @@ CurrentLoop:
 		}
 	}
 	if prime {
+		p.threshold = uint64(math.Sqrt(float64(p.n))) + 1
 		p.primes = append(p.primes, p.n)
 		p.current = append(p.current, p.n)
 	}
