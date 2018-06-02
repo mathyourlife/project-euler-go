3374032630341cf95d9d04e190e12f62af97268d
Dan Couture <mathyourlife@gmail.com>
2018-06-02 15:08:15 -0400

    re-solved 28 in a much more efficient manner

--- pkg/problems/solutions_test.go
+++ pkg/problems/solutions_test.go
@@ -38,7 +38,7 @@ func TestSolutions(t *testing.T) {
 		{&ThousandDigitFibonacciNumber{}, "4782"},
 		{&ReciprocalCycles{}, "983"},
 		{&QuadraticPrimes{}, "-59231"},
-		// 669171001
+		{&NumberSpiralDiagonals{}, "669171001"},
 		// 9183
 		// 443839
 		// 73682
